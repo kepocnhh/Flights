e564a65d21d3801fa23885f7cf36f32fcd3eb7aa
Stanley Wintergreen <kepocnhh@gmail.com>
2024-08-18 22:48:17 +0300

    settings...

--- app/src/main/kotlin/org/kepocnhh/flights/module/app/Colors.kt
+++ app/src/main/kotlin/org/kepocnhh/flights/module/app/Colors.kt
@@ -2,13 +2,14 @@ package org.kepocnhh.flights.module.app
 
 import androidx.compose.ui.graphics.Color
 
-internal sealed class Colors(
-    val primary: Color,
-    val black: Color,
-    val white: Color,
-    val background: Color,
-    val foreground: Color,
-) {
+internal sealed interface Colors {
+    val primary: Color
+    val black: Color
+    val white: Color
+    val background: Color
+    val foreground: Color
+    val secondary: Color
+    val text: Color
 
     enum class Type {
         Auto,
@@ -16,19 +17,23 @@ internal sealed class Colors(
         Light,
     }
 
-    data object Light : Colors(
-        primary = Color(0xff1E88E5),
-        white = Color(0xffffffff),
-        foreground = Color(0xff1f1f1f),
-        background = Color(0xffefefef),
-        black = Color(0xff000000),
-    )
+    data object Light : Colors {
+        override val primary = Color(0xff1E88E5)
+        override val white = Color(0xffffffff)
+        override val foreground = Color(0xff1f1f1f)
+        override val secondary = Color(0xffdfdfdf)
+        override val background = Color(0xffefefef)
+        override val black = Color(0xff000000)
+        override val text = black
+    }
 
-    data object Dark : Colors(
-        primary = Color(0xff1E88E5),
-        black = Color(0xff000000),
-        background = Color(0xff1f1f1f),
-        foreground = Color(0xffefefef),
-        white = Color(0xffffffff),
-    )
+    data object Dark : Colors {
+        override val primary = Color(0xff1E88E5)
+        override val black = Color(0xff000000)
+        override val background = Color(0xff1f1f1f)
+        override val secondary = Color(0xff2f2f2f)
+        override val foreground = Color(0xffefefef)
+        override val white = Color(0xffffffff)
+        override val text = white
+    }
 }
--- app/src/main/kotlin/org/kepocnhh/flights/module/settings/SettingsScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/flights/module/settings/SettingsScreen.kt
@@ -2,11 +2,22 @@ package org.kepocnhh.flights.module.settings
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.kepocnhh.flights.App
+import org.kepocnhh.flights.BuildConfig
 
 @Composable
 internal fun SettingsScreen(
@@ -18,6 +29,27 @@ internal fun SettingsScreen(
             .fillMaxSize()
             .background(App.Theme.colors.background),
     ) {
-        // todo
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                SettingsVersion(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    versionName = BuildConfig.VERSION_NAME,
+                    versionCode = BuildConfig.VERSION_CODE,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
     }
 }
